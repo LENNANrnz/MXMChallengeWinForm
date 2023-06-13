cd3ba5f603d225f8f51ae453d8f7e3b99508d6c8
Lennan Renzo <lennancontato@gmail.com>
2023-06-13 20:25:35 -0300

    chore: refactoring

--- TaskManager/Form1.cs
+++ TaskManager/Form1.cs
@@ -19,14 +19,11 @@ namespace TaskManager
         {
             InitializeComponent();
         }
-
         private void Form1_Load(object sender, EventArgs e)
         {
             ListAllProcesses();
             timer.Start();
         }
-
-
         public void ListAllProcesses()
         {
             Process[] allProcess = Process.GetProcesses();
@@ -41,37 +38,21 @@ namespace TaskManager
                 cpuCounter.NextValue();
                 ramCounter.NextValue();
                 networkUsageCounter.NextValue();
-
-               
+                               
                 System.Threading.Thread.Sleep(100);
 
                 float ramUsage = ramCounter.NextValue() / (1024 * 1024); 
                 float networkUsage = networkUsageCounter.NextValue() / (1024 * 1024); 
-
                 float totalRam = totalRamCounter.NextValue() / (1024 * 1024); 
                 float ramUsagePercentage = (ramUsage / totalRam) * 100;
-
                 float cpuUsage = cpuCounter.NextValue();
 
                 string cpuUsageFormatted = cpuUsage.ToString("N2") + "%";
                 string ramUsageFormatted = ramUsagePercentage.ToString("N2") + "%";
-
-              
-                  
-                    tabeladados.Rows.Add(pl.ProcessName, pl.Id, ramUsageFormatted, cpuUsageFormatted, networkUsage.ToString("N2") + " MB/s");
-                
-
-
-            }
-
+                            
+                tabeladados.Rows.Add(pl.ProcessName, pl.Id, ramUsageFormatted, cpuUsageFormatted, networkUsage.ToString("N2") + " MB/s");
+            }    
         }
-
-
-
-
-
-
-
         private void timer_Tick(object sender, EventArgs e)
         {
             float fcpu = pCPU.NextValue();
@@ -80,16 +61,12 @@ namespace TaskManager
             float fram = pRAM.NextValue();
             progressBarRAM.Value = (int)fram;
             lblRAM.Text = string.Format("{0:0.00}%", fram);
-
          
             DateTime currentTime = DateTime.Now;
             string formattedDateTime = currentTime.ToString("HH:mm");
-
-           
+       
             chart1.Series["CPU"].Points.AddXY(formattedDateTime, fcpu);
             chart1.Series["RAM"].Points.AddXY(formattedDateTime, fram);
-
-          
 
         }
     }
