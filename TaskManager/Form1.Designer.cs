2ba671920efe7d77a26d9d7bc709fc46a368dea2
Lennan Renzo <lennancontato@gmail.com>
2023-06-13 17:19:32 -0300

    feat: grafico feito

--- TaskManager/Form1.Designer.cs
+++ TaskManager/Form1.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series8 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.progressBarCPU = new System.Windows.Forms.ProgressBar();
@@ -38,15 +42,17 @@
             this.pCPU = new System.Diagnostics.PerformanceCounter();
             this.timer = new System.Windows.Forms.Timer(this.components);
             this.pRAM = new System.Diagnostics.PerformanceCounter();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             ((System.ComponentModel.ISupportInitialize)(this.pCPU)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pRAM)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(244, 86);
+            this.label1.Location = new System.Drawing.Point(22, 25);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(60, 25);
             this.label1.TabIndex = 0;
@@ -56,7 +62,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(244, 166);
+            this.label2.Location = new System.Drawing.Point(22, 63);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(62, 25);
             this.label2.TabIndex = 1;
@@ -64,14 +70,14 @@
             // 
             // progressBarCPU
             // 
-            this.progressBarCPU.Location = new System.Drawing.Point(323, 87);
+            this.progressBarCPU.Location = new System.Drawing.Point(101, 26);
             this.progressBarCPU.Name = "progressBarCPU";
             this.progressBarCPU.Size = new System.Drawing.Size(267, 23);
             this.progressBarCPU.TabIndex = 2;
             // 
             // progressBarRAM
             // 
-            this.progressBarRAM.Location = new System.Drawing.Point(323, 168);
+            this.progressBarRAM.Location = new System.Drawing.Point(101, 65);
             this.progressBarRAM.Name = "progressBarRAM";
             this.progressBarRAM.Size = new System.Drawing.Size(267, 23);
             this.progressBarRAM.TabIndex = 3;
@@ -80,7 +86,7 @@
             // 
             this.lblCPU.AutoSize = true;
             this.lblCPU.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCPU.Location = new System.Drawing.Point(621, 87);
+            this.lblCPU.Location = new System.Drawing.Point(399, 26);
             this.lblCPU.Name = "lblCPU";
             this.lblCPU.Size = new System.Drawing.Size(41, 25);
             this.lblCPU.TabIndex = 4;
@@ -90,7 +96,7 @@
             // 
             this.lblRAM.AutoSize = true;
             this.lblRAM.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblRAM.Location = new System.Drawing.Point(621, 166);
+            this.lblRAM.Location = new System.Drawing.Point(399, 63);
             this.lblRAM.Name = "lblRAM";
             this.lblRAM.Size = new System.Drawing.Size(41, 25);
             this.lblRAM.TabIndex = 5;
@@ -112,11 +118,34 @@
             this.pRAM.CategoryName = "Memory";
             this.pRAM.CounterName = "% Committed Bytes In Use";
             // 
+            // chart1
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(27, 143);
+            this.chart1.Name = "chart1";
+            series7.ChartArea = "ChartArea1";
+            series7.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series7.Legend = "Legend1";
+            series7.Name = "CPU";
+            series7.YValuesPerPoint = 4;
+            series8.ChartArea = "ChartArea1";
+            series8.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series8.Legend = "Legend1";
+            series8.Name = "RAM";
+            this.chart1.Series.Add(series7);
+            this.chart1.Series.Add(series8);
+            this.chart1.Size = new System.Drawing.Size(538, 365);
+            this.chart1.TabIndex = 6;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1446, 521);
+            this.ClientSize = new System.Drawing.Size(1446, 616);
+            this.Controls.Add(this.chart1);
             this.Controls.Add(this.lblRAM);
             this.Controls.Add(this.lblCPU);
             this.Controls.Add(this.progressBarRAM);
@@ -128,6 +157,7 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pCPU)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pRAM)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -144,6 +174,7 @@
         private System.Diagnostics.PerformanceCounter pCPU;
         private System.Windows.Forms.Timer timer;
         private System.Diagnostics.PerformanceCounter pRAM;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
 
--- TaskManager/Form1.cs
+++ TaskManager/Form1.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace TaskManager
 {
@@ -28,11 +29,22 @@ namespace TaskManager
             float fcpu = pCPU.NextValue();
             progressBarCPU.Value = (int)fcpu;
             lblCPU.Text = string.Format("{0:0.00}%", fcpu);
-
             float fram = pRAM.NextValue();
             progressBarRAM.Value = (int)fram;
             lblRAM.Text = string.Format("{0:0.00}%", fram);
 
+            //chart1.Series["CPU"].Points.AddY(fcpu);
+            //chart1.Series["RAM"].Points.AddY(fram);
+
+            // Criando um objeto DateTime com a hora e minuto atual
+            DateTime currentTime = DateTime.Now;
+            string formattedDateTime = currentTime.ToString("HH:mm");
+
+            // Adicionando um ponto de dados para o eixo X usando o objeto DateTime
+            chart1.Series["CPU"].Points.AddXY(formattedDateTime, fcpu);
+            chart1.Series["RAM"].Points.AddXY(formattedDateTime, fram);
+
+          
 
         }
     }
